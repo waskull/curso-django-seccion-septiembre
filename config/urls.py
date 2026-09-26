2d8d88b33442cc0409a0b1553377b3e1c917b7c9
Martin <mrtncsto@gmail.com>
2026-09-26 16:10:32 -0400

    clase4

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from ubicaciones.views import EstadoView, EstadoViewDetail
+from ubicaciones.views import CiudadViewSet, EstadoView, EstadoViewDetail
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'ciudad', basename="ciudades", viewset=CiudadViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('estado/', name='estados', view=EstadoView.as_view()),
-    path('estado/<int:pk>/', name="estado", view=EstadoViewDetail.as_view())
+    path('estado/<int:pk>/', name="estado", view=EstadoViewDetail.as_view()),
+    path('', include(router.urls))
 ]
--- ubicaciones/views.py
+++ ubicaciones/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from ubicaciones.models import Agencia, Ciudad, Estado
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from rest_framework import status
 
 # Create your views here.
@@ -31,7 +32,6 @@ class EstadoView(APIView):
 
 class EstadoViewDetail(APIView):
     def get(self, request, pk=None):
-        #Primero valido si existe ese registro
         try:
             registro = Estado.objects.get(pk=pk)
         except Estado.DoesNotExist:
@@ -43,3 +43,76 @@ class EstadoViewDetail(APIView):
 
         return Response(registro, status=status.HTTP_200_OK)
 
+    def delete(self, request, pk=None):
+            try:
+                registro = Estado.objects.get(pk=pk)
+                registro.delete()
+            except Estado.DoesNotExist:
+                return Response({"mensaje":"Ese estado no existe"}, status=status.HTTP_404_NOT_FOUND)
+    
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk=None):
+        nombre = self.request.data["nombre"]
+        if not nombre:
+            return Response({"mensaje":"El nombre es requerido"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            registro = Estado.objects.get(pk=pk)
+            registro.nombre = nombre
+            registro.save()
+        except Estado.DoesNotExist:
+            return Response({"mensaje":"Ese estado no existe"}, status=status.HTTP_404_NOT_FOUND)
+
+
+        return Response({"mensaje":"El registro se ha editado"}, status=status.HTTP_200_OK)
+
+    def patch(self,request,pk=None):
+        return self.put(request=request,pk=pk)
+
+
+from rest_framework.viewsets import ViewSet
+
+class CiudadViewSet(ViewSet):
+    def list(self, request):
+        data = [
+            {
+                "id": ciudad.pk,
+                "nombre": ciudad.nombre,
+                "estado": ciudad.estado.nombre
+            } for ciudad in Ciudad.objects.all()
+        ]
+
+        return Response(data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+            try:
+                registro = Ciudad.objects.get(pk=pk)
+            except Ciudad.DoesNotExist:
+                return Response({"mensaje":"Esa ciudad no existe"}, status=status.HTTP_404_NOT_FOUND)
+            registro = {
+                "id": registro.id,
+                "nombre": registro.nombre,
+                "estado":registro.estado.nombre
+            }
+    
+            return Response(registro, status=status.HTTP_200_OK)
+
+    def create(self, request): #post para crear registros
+        datos = self.request.data.copy()
+        id_estado = datos["estado"]
+        estado = Estado.objects.get(id=id_estado)
+        existe = Ciudad.objects.filter(nombre__icontains=datos["nombre"], estado=estado).exists()
+
+        if existe:
+            return Response({"mensaje":f"{datos["nombre"]} ya existe registrada en el estado {estado.nombre}"}, status=status.HTTP_400_BAD_REQUEST)
+
+        Ciudad.objects.create(estado=estado,nombre=datos["nombre"])
+
+        return Response({"mensaje":f"La ciudad {datos["nombre"]} ha sido creada"}, status=status.HTTP_201_CREATED)
+
+    def update(self,request,pk=None): #metodo http para editar todo un recurso. put
+        pass
+    def partial_update(self,request,pk=None): #metodo para editar un recurso parcialmente, patch
+        pass
+    def delete(self,request,pk=None): #delete
+        ...
